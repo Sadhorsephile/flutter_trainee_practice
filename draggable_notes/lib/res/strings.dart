8a7b01325c79aa89073cd412b4155091cc7990e3
AndrewVorotyntsev <andrey12092000@gmail.com>
2023-07-13 15:50:28 +0300

    SI-4488 feat(notes): строки

---
+++ draggable_notes/lib/res/strings.dart
@@ -0,0 +1,11 @@
+class AppStrings {
+  static const notes = 'Заметки';
+  static const addNote = 'Добавить заметку';
+  static const newNote = 'Новая заметка';
+  static const title = 'Название';
+  static const enterTitle = 'Введите навание';
+  static const content = 'Содержание';
+  static const enterContent = 'Введите содержание';
+  static const save = 'Сохранить';
+  static const cancel = 'Отмена';
+}
