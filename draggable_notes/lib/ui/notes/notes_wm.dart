1dc6de83e389a0a422bfabfce352b57574be67ef
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-07-14 12:39:25 +0300

    SI-4489 feat(notes): Виджет модель для экрана заметок

--- draggable_notes/lib/ui/notes/notes_wm.dart
+++ draggable_notes/lib/ui/notes/notes_wm.dart
@@ -1,21 +1,28 @@
+import 'package:draggable_notes/data/note.dart';
+import 'package:draggable_notes/di/di_container.dart';
+import 'package:draggable_notes/interactor/notes_interactor.dart';
 import 'package:draggable_notes/ui/notes/notes_model.dart';
 import 'package:draggable_notes/ui/notes/notes_screen.dart';
 import 'package:draggable_notes/ui/widgets/create_note_dialog.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
 
-/// Абстракция Widget Model
+/// Абстракция Widget Model для экрана заметок
 abstract class INotesWidgetModel extends IWidgetModel {
-  /// Сохранение заметки
-  void onSaveTap();
+  ListenableState<EntityState<List<NoteDomain>>> get notesListState;
 
-  /// Обработка перетаскивания
-  void handleDrag(int oldIndex, int newIndext);
+  /// Добавление заметки
+  void onCreateNoteTap();
+
+  /// Обработка перетаскивания со старого места [oldIndex] на новое [newIndex]
+  void handleDrag(int oldIndex, int newIndex);
 }
 
 NotesWidgetModel defaultAppWidgetModelFactory(BuildContext context) {
   return NotesWidgetModel(
-    NotesScreenModel(),
+    NotesScreenModel(
+      getIt.get<NotesInteractor>(),
+    ),
   );
 }
 
@@ -24,24 +31,61 @@ class NotesWidgetModel extends WidgetModel<NotesScreen, NotesScreenModel>
     implements INotesWidgetModel {
   NotesWidgetModel(super._model);
 
+  late final TextEditingController titleEditingController;
+  late final TextEditingController contentEditingController;
+
   @override
-  void handleDrag(int oldIndex, int newIndext) {
-    // TODO(AndrewVorotyntsev):  implement handleDrag
+  EntityStateNotifier<List<NoteDomain>> notesListState =
+      EntityStateNotifier<List<NoteDomain>>();
+
+  @override
+  void initWidgetModel() {
+    titleEditingController = TextEditingController();
+    contentEditingController = TextEditingController();
+    getPhoto();
+    super.initWidgetModel();
+  }
+
+  void getPhoto() {
+    final notes = model.getNotes();
+    notesListState.content(notes);
   }
 
   @override
-  void onSaveTap() {
+  void dispose() {
+    titleEditingController.dispose();
+    contentEditingController.dispose();
+    notesListState.dispose();
+    super.dispose();
+  }
+
+  @override
+  void handleDrag(int oldIndex, int newIndex) {
+    model.replaceNotes(oldIndex, newIndex);
+    getPhoto();
+  }
+
+  @override
+  void onCreateNoteTap() {
     showDialog(
       context: context,
       builder: (context) {
         return CreateNoteDialog(
-          titleEditingController: TextEditingController(),
-          contentEditingController: TextEditingController(),
-          onSaveTap: () {
-            // TODO(AndrewVorotyntsev): impement
-          },
+          titleEditingController: titleEditingController,
+          contentEditingController: contentEditingController,
+          onSaveTap: _saveNote,
         );
       },
     );
+  }
+
+  void _saveNote() {
+    model.addNote(NoteDomain(
+      title: titleEditingController.text,
+      content: contentEditingController.text,
+    ));
+    titleEditingController.text = '';
+    contentEditingController.text = '';
+    getPhoto();
   }
 }
