6d0aab201866eb42a80404df9307a658e6478989
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-07-18 15:02:14 +0300

    SI-4489 fix(notes): Аннотации для формата данных

--- draggable_notes/lib/storage/notes/hive/adapters/note.dart
+++ draggable_notes/lib/storage/notes/hive/adapters/note.dart
@@ -6,9 +6,11 @@ part 'note.g.dart';
 /// Описание сущности заметки, которая будет храниться в Hive
 @HiveType(typeId: 0)
 class NoteDB extends HiveObject implements NoteData {
+  @override
   @HiveField(0)
   final String title;
 
+  @override
   @HiveField(1)
   final String content;
 
