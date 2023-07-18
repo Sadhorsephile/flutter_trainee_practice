e2bde6e1729d830c8359fb4e5f344f2ba8417a84
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-07-18 14:46:14 +0300

    SI-4489 feat(notes): Формат хранения заметки

--- draggable_notes/lib/storage/notes/notes_storage.dart
+++ draggable_notes/lib/storage/notes/notes_storage.dart
@@ -2,7 +2,7 @@ import 'package:draggable_notes/data/note.dart';
 
 /// Хранилище для работы с заметками
 /// Оно работает с заметками в формате [T]
-abstract class NotesStorage<T> {
+abstract class NotesStorage<T extends NoteData> {
   /// Получить заметки
   List<T> getNotes();
 
@@ -11,4 +11,12 @@ abstract class NotesStorage<T> {
 
   /// Поменять заметки местами
   void replaceNotes(int oldIndex, int newIndex);
+}
+
+/// Формат хранения заметки
+/// Может быть в любой форме
+/// Должен предоставлять геттеры для строк заголовка и контента
+abstract class NoteData {
+  String get title;
+  String get content;
 }
