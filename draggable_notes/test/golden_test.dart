ec0612de935ba70edd151b8d8d5e834bb4ecbe85
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-07-19 19:03:16 +0300

    SI-4492 Golden тесты для диалога выбора темы

--- draggable_notes/test/golden_test.dart
+++ draggable_notes/test/golden_test.dart
@@ -1,9 +1,15 @@
+import 'package:draggable_notes/providers/theme_provider.dart';
 import 'package:draggable_notes/res/themes.dart';
 import 'package:draggable_notes/ui/notes/notes_screen.dart';
 import 'package:draggable_notes/ui/widgets/create_note_dialog.dart';
 import 'package:draggable_notes/ui/widgets/note_card.dart';
+import 'package:draggable_notes/ui/widgets/theme_picker_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+
+import 'mock/mock_notes_interactor.dart';
 
 void main() {
   testGoldens('Notes screen (Light theme)', (tester) async {
@@ -62,6 +68,36 @@ void main() {
     await screenMatchesGolden(tester, 'create_new_note_dialog_light');
   });
 
+  testGoldens('Theme picker dialog (Light theme)', (tester) async {
+    // Явно указывать тип после final иначе тест не проходит
+    final ThemeProvider themeProver = ThemeProviderMock();
+
+    when(
+      () => themeProver.currentThemeMode,
+    ).thenAnswer(
+      (_) => ThemeMode.light,
+    );
+    final builder = GoldenBuilder.column()
+      ..addScenario(
+        'theme_picker_dialog_light',
+        ChangeNotifierProvider(
+          create: (_) => themeProver,
+          child: Theme(
+            data: AppThemes.lightTheme,
+            child: Builder(builder: (context) {
+              return ThemePickerDialog(
+                onThemeChanged: (_) {},
+              );
+            }),
+          ),
+        ),
+      );
+
+    await tester.pumpWidgetBuilder(builder.build());
+
+    await screenMatchesGolden(tester, 'theme_picker_dialog_light');
+  });
+
   testGoldens('Notes screen (Dark Mode)', (tester) async {
     final builder = DeviceBuilder()
       ..overrideDevicesForAllScenarios(
@@ -116,5 +152,34 @@ void main() {
     await tester.pumpWidgetBuilder(builder.build());
 
     await screenMatchesGolden(tester, 'create_new_note_dialog_dark');
+  });
+
+  testGoldens('Theme picker dialog (Dark theme)', (tester) async {
+    // Явно указывать тип после final иначе тест не проходит
+    final ThemeProvider themeProver = ThemeProviderMock();
+    when(
+      () => themeProver.currentThemeMode,
+    ).thenAnswer(
+      (_) => ThemeMode.dark,
+    );
+    final builder = GoldenBuilder.column()
+      ..addScenario(
+        'theme_picker_dialog_dark',
+        ChangeNotifierProvider(
+          create: (_) => themeProver,
+          child: Theme(
+            data: AppThemes.darkTheme,
+            child: Builder(builder: (context) {
+              return ThemePickerDialog(
+                onThemeChanged: (_) {},
+              );
+            }),
+          ),
+        ),
+      );
+
+    await tester.pumpWidgetBuilder(builder.build());
+
+    await screenMatchesGolden(tester, 'theme_picker_dialog_dark');
   });
 }
--- draggable_notes/test/mock/mock_notes_interactor.dart
+++ draggable_notes/test/mock/mock_notes_interactor.dart
@@ -1,5 +1,8 @@
 import 'package:draggable_notes/interactor/notes/notes_interactor.dart';
+import 'package:draggable_notes/providers/theme_provider.dart';
 import 'package:mocktail/mocktail.dart';
 
 /// Мок для [NotesInteractor].
 class NotesInteractorMock extends Mock implements NotesInteractor {}
+
+class ThemeProviderMock extends Mock implements ThemeProvider {}
