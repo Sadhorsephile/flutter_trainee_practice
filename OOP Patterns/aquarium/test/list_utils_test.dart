1adb9d81420512f4eed94af7057ec46f9e005e67
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-07-25 14:32:16 +0300

    SI-4557 refactor(fish): Рефакторинг тестов для расширения над списком

--- OOP Patterns/aquarium/test/list_utils_test.dart
+++ OOP Patterns/aquarium/test/list_utils_test.dart
@@ -2,7 +2,10 @@ import 'package:aquarium/utils/list_utils.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  /// Тесты для проверки расширения над списком,
+  /// которое возвращает случайный элемент.
   group('RandomItem Extension test', () {
+    /// Случай пустого списка
     test('no element', () {
       final list = [];
       final randomItem = list.getRandom();
@@ -10,15 +13,16 @@ void main() {
       expect(randomItem, null);
     });
 
+    /// Работа с одним элементом
     test('one element', () {
       const onlyOneItem = 'item1';
-      final list = [];
-      list.add(onlyOneItem);
+      final list = [onlyOneItem];
       final randomItem = list.getRandom();
 
       expect(randomItem, onlyOneItem);
     });
 
+    /// Работа с двумя элементами
     test('two elements', () {
       const item1 = 'item1';
       const item2 = 'item2';
@@ -28,6 +32,7 @@ void main() {
       expect(randomItem, predicate((item) => item == item1 || item == item2));
     });
 
+    /// Проверка того, что возвращенный элемент содержится в списке
     test('test contains element', () {
       final list = List.generate(10, (index) => index);
       final randomItem = list.getRandom();
